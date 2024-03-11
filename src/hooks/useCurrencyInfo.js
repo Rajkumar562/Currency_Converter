854dee36f3a8c39556420d823e0c5a3450c54af3
Rajkumar <rajkumarchoudhury562@gmail.com>
2024-03-12 00:35:50 +0530

    committing files

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,16 @@
+//hooks are generally given the js type because they return a js value.Files should be named according to the value they return. Jsx should be named when they return the given object.
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024.3.5/v1/currencies/${currency}.json`)
+      .then((res) => res.json()) // converting data is into json because generally api gives output in string
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+  console.log(data);
+  return data;
+}
+
+export default useCurrencyInfo;
